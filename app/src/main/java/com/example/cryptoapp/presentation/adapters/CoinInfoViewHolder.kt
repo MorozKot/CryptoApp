f333a515ae91e1bae5b1a06d4d354648356659a2
Vladislav Moroz <moroz.vlad2012@gmail.com>
2022-05-11 13:55:53 +0200

    ViewBinding in Adapter

---
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.databinding.ItemCoinInfoBinding
+import com.example.cryptoapp.domain.CoinInfo
+
+/**
+ *@author Moroz V.A. on 11.05.2022
+ **/
+class CoinInfoViewHolder (
+    val binding: ItemCoinInfoBinding
+) : RecyclerView.ViewHolder(binding.root)
+
